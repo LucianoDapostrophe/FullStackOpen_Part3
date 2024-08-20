780c49c4be3cf2c2b9dae52067b955ca04319685
LucianoDapostrophe <90058982+LucianoDapostrophe@users.noreply.github.com>
2024-08-20 13:44:08 -0600

    3.8

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('messageBody', (request, response) =>{
+    return JSON.stringify(request.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :messageBody'))
 
 let persons = [
     { 
